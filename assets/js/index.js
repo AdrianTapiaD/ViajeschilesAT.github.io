595f24885d43ee1cff89d54dc50256c523575b68
AdrianTapiaD <adrian.tapia.donoso@gmail.com>
2024-02-13 16:52:21 -0300

    v8

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,14 @@ $("#enviar_correo").click(function(){
     alert("El correo fue enviado correctamente.");
     });
 
-    $("h5").on("dblclick", function(){
-        $(this).css('color', 'blue');
-      });
-      $("h5").on("click", function(){
-        $(this).css('color', 'white');
-      });
+$("h2").on("click", function(){
+    $(this).css('color', 'blue');
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var clickableHeading = document.querySelector('.clickable-heading');
+
+        clickableHeading.addEventListener('click', function () {
+            clickableHeading.classList.add('blue-text');
+        });
+    });
