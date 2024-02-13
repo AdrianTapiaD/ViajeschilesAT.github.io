609cea887808e7bd7b86dfd7811754eae0ad6b73
AdrianTapiaD <adrian.tapia.donoso@gmail.com>
2024-02-13 16:29:18 -0300

    v7

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,10 @@
 $("#enviar_correo").click(function(){
     alert("El correo fue enviado correctamente.");
     });
+
+    $("h5").on("dblclick", function(){
+        $(this).css('color', 'blue');
+      });
+      $("h5").on("click", function(){
+        $(this).css('color', 'white');
+      });
